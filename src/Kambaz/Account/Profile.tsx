1e2644ee81064d55aff96cd0741de7717fbcfd1d
Ana Bordeianu <116093824+ana-bordeianu@users.noreply.github.com>
2025-02-19 21:39:19 -0500

    class to classname

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -3,7 +3,7 @@ export default function Profile() {
 	return (
 		<div id="wd-profile-screen">
 			<h3>Profile</h3>
-			<input defaultValue="alice" placeholder="username" class="wd-username"/><br/>
+			<input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
 			<input defaultValue="123" placeholder="password" type="password"
 			class="wd-password" /><br/>
 			<input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>

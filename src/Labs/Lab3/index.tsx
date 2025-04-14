a59c57b76ab06c45246e0d14ae56b389090c73ac
ana-bordeianu <ana.e.bordo@gmail.com>
2025-04-14 18:07:44 -0400

    Lab 3.4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -27,6 +27,8 @@ import Spreading from "./Spreading";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
 
 export default function Lab3() {
 	return (
@@ -59,5 +61,7 @@ export default function Lab3() {
 		<Destructing/>
 		<FunctionDestructing/>
 		<DestructingImports/>
+		<Classes/>
+		<Styles/>
   </div>
   )};

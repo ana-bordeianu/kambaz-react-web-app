7c83b222afe6cc798e538d08932d5ba8a582f80e
ana-bordeianu <ana.e.bordo@gmail.com>
2025-04-22 23:02:16 -0400

    Finished Lab 5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -3,6 +3,9 @@ import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import "./index.css";
 
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
@@ -22,6 +25,9 @@ export default function Lab5() {
             <QueryParameters />
             <WorkingWithObjects />
             <WorkingWithArrays />
+            <HttpClient />
+            <WorkingWithObjectsAsynchronously />
+            <WorkingWithArraysAsynchronously />
         </div>
     );
 }
